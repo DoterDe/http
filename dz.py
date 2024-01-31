959b221eb8768b9ed6ba992b67a8a218d3590e96
DoterDe <153744082+DoterDe@users.noreply.github.com>
2024-01-31 21:46:37 +0600

    Update dz.py

--- dz.py
+++ dz.py
@@ -4,19 +4,20 @@ import asyncio
 import os
 #1
 os.mkdir('dz')
-response=requests.get('https://jsonplaceholder.typicode.com/photos/1')
-with open('./dz/git.json','w') as file:
-    file.write(response.text)
+for i in range(1,101):
+    response=requests.get(f'https://jsonplaceholder.typicode.com/photos/{i}')
+    with open(f'./dz/git{i}.json','w') as file:
+        file.write(response.text)
 
 #2
 os.mkdir('dz_1')
 
 async def download_json(url, session):
     async with session.get(url) as response:
-        with open("./dz_1/image_.json", 'w') as file:
+        with open(f"./dz_1/image{url[-1]}.json", 'w') as file:
             file.write(await response.text())
 async def save_image():
-    urls=["https://jsonplaceholder.typicode.com/photos/1"]
+    urls=[f"https://jsonplaceholder.typicode.com/photos/{i}" for i in range(1,101)]
     async with aiohttp.ClientSession() as session:
         reponse = [download_json(url,session)for url in urls]
         await asyncio.gather(*reponse)
